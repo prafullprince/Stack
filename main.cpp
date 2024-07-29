601bfc224830e2dc24a7533d56d0844022ccb91d
Prafull Prince <159343572+prafullprince@users.noreply.github.com>
2024-07-29 07:53:14 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+#include <stack>
+
+
+
+//             stack function using from STL
+
+
+
+
+// int main() {
+
+//   // stack creation    ,
+//   stack<int> st;
+
+//   // stack insertion
+//   st.push(10);
+//   st.push(20);
+//   st.push(30);
+
+//   // stack size
+//   cout<<st.size()<<endl;
+
+//   // stack deletion
+//   cout<<st.top()<<endl;
+//   st.pop();
+
+//   // top element
+//   cout<<st.top()<<endl;
+//   st.pop();
+//   cout<<st.top()<<endl;
+//   st.pop();
+//   cout<<st.top()<<endl;
+
+//   // stack empty
+//   if(st.empty()){
+//     cout<<"stack is empty"<<endl;
+//   }else{
+//     cout<<"stack is not empty"<<endl;
+//   }
+  
+// }
