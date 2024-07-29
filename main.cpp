e3fbdb66c00a644250aa043eea1fb514a1a398cf
Prafull Prince <159343572+prafullprince@users.noreply.github.com>
2024-07-29 07:54:59 +0000

    stack creation using STL

--- main.cpp
+++ main.cpp
@@ -9,35 +9,30 @@ using namespace std;
 
 
 
-// int main() {
-
-//   // stack creation    ,
-//   stack<int> st;
-
-//   // stack insertion
-//   st.push(10);
-//   st.push(20);
-//   st.push(30);
-
-//   // stack size
-//   cout<<st.size()<<endl;
-
-//   // stack deletion
-//   cout<<st.top()<<endl;
-//   st.pop();
-
-//   // top element
-//   cout<<st.top()<<endl;
-//   st.pop();
-//   cout<<st.top()<<endl;
-//   st.pop();
-//   cout<<st.top()<<endl;
-
-//   // stack empty
-//   if(st.empty()){
-//     cout<<"stack is empty"<<endl;
-//   }else{
-//     cout<<"stack is not empty"<<endl;
-//   }
+int main() {
+  // stack creation    ,
+  stack<int> st;
+  // stack insertion
+  st.push(10);
+  st.push(20);
+  st.push(30);
+  // stack size
+  cout<<st.size()<<endl;
+  // stack deletion
+  cout<<st.top()<<endl;
+  st.pop();
+  // top element
+  cout<<st.top()<<endl;
+  st.pop();
+  cout<<st.top()<<endl;
+  st.pop();
+  cout<<st.top()<<endl;
+  cout<<st.size()<<endl;
+  // stack empty
+  if(st.empty()){
+    cout<<"stack is empty"<<endl;
+  }else{
+    cout<<"stack is not empty"<<endl;
+  }
   
-// }
+}
